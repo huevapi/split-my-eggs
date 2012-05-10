869fa5a25fbd910963233911926e7f0ad5ac15cf
Sebastian Acuna <sacuna@gmail.com>
2012-05-09 22:38:34 -0400

    Uniqueness of user emails

--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
-module.exports=function User(mongoose) {
+module.exports = function User(mongoose) {
 	var Schema = mongoose.Schema
 	  , ObjectId = Schema.ObjectId;
 	var schema = new Schema({
-    email			:  { type: String }
+    email			:  { type: String, index: {unique: true, dropDups: true} }
   , name 			:  { type: String }
 	});
-	var model=mongoose.model('User', schema);
+	var model = mongoose.model('User', schema);
 	return model;
 };
